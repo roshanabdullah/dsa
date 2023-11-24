abfc77f292bb9f785db6b5ba35f2cf7f5617c1eb
Abdullah Roshan <85522748+roshanabdullah@users.noreply.github.com>
2023-11-24 12:00:48 +0500

    Create minDepthTree.js

---
+++ minDepthTree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+    if (root == null) return 0;
+
+    let leftDepth = minDepth(root.left);
+    let rightDepth = minDepth(root.right);
+
+    if( leftDepth === 0 || rightDepth === 0) {
+      return (leftDepth + rightDepth) + 1;
+    }
+    else {
+      return (leftDepth < rightDepth ? leftDepth : rightDepth) + 1
+    }
+};
