0b2e6d25502ffe4dd7a9cfb2eaeafc2bb804eede
Abdullah Roshan <85522748+roshanabdullah@users.noreply.github.com>
2023-12-04 15:40:48 +0500

    Update dynamic-p.js

--- dynamic-p.js
+++ dynamic-p.js
@@ -16,3 +16,26 @@ function memoizedAddTo80(n) {
 const memoized = memoizedAddTo80();
 console.log('1:', memoized(5))
 console.log('2:', memoized(10))
+
+
+
+// Common Substring
+// Note: please restart the page if syntax highlighting works bad.
+function commonSubstring(s) {
+
+  let charCount = {};
+  let commonChars = [];
+  for (let i=0; i<s.length; i++) {
+    if(charCount[s[i]]) {
+      commonChars.push(s[i])
+    }
+
+    else {
+      charCount[s[i]] = 1;
+    }
+  }
+
+  return commonChars;
+}
+
+console.log(commonSubstring("Hello"))
