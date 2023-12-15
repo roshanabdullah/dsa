15a3d5d21044a1a7657d56c7c0b33ecaed3df78c
Abdullah Roshan <85522748+roshanabdullah@users.noreply.github.com>
2023-12-15 13:30:18 +0500

    Update dynamic-p.js

--- dynamic-p.js
+++ dynamic-p.js
@@ -39,3 +39,41 @@ function commonSubstring(s) {
 }
 
 console.log(commonSubstring("Hello"))
+
+// Fibnocci Series
+// memoization method (top down approach)
+
+function FibnocciDynamicVersion(){
+    let cache = {}
+    
+    return function fib(n) {
+        if(n in cache) {
+            return cache[n]
+        }
+        else {
+            if(n < 2) {
+                return n;
+            }
+            
+            else {
+                cache[n] = fib(n - 1) + fib(n - 2)
+                return cache[n]
+            }
+        }
+    }
+}
+
+
+// bottom up approach
+function FibMaster2(n) {
+    let answer = [0, 1]
+    
+    for (let i=2; i<=n; i++) {
+        answer.push(answer[i - 2] + answer[i - 1])
+    }
+    return answer.pop()
+}
+
+const fasterFib = FibnocciDynamicVersion();
+console.log('DP2: ', FibMaster2(100))
+console.log('DP: ', fasterFib(100))
